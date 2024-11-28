1f45c4b94bd11974fb5bb6ec080f4ed339a32e1b
Martin <martin.elfantastico@gmail.com>
2024-11-28 23:52:08 +0100

    javascript exercise-2

---
+++ js-class/js-introduction-class-1/exercise-2/assets/app.js
@@ -0,0 +1,30 @@
+// Exercise 2 
+
+// A.
+
+const pi = 3.142;
+
+let r;
+
+a = pi * (r * 2);
+
+// B.
+
+console.log(a);
+
+// C.
+
+let c = 100
+
+// D.
+
+r = c / ( 2 * pi );
+
+// E.
+
+d = c / pi;
+
+
+// F.
+
+console.log(`The circle has circumference of ${c}m. Its radius is ${r} and the diameter is ${d}.`);
