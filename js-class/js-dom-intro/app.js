d53ef75d3883b6ea38e285d3a4e6c8438b66adbb
Martin <martin.elfantastico@gmail.com>
2024-12-10 18:16:08 +0100

    Exercise 1

---
+++ js-class/js-dom-intro/app.js
@@ -0,0 +1,11 @@
+//Da vrakja niza pomeju tie dva elementi
+
+function range(start, end, step) {
+    const array = []
+    for(let i = start; i < end; i += step){
+        array.push[i]
+    }
+    return array
+}
+
+console.log(range(1, 10, 2))
