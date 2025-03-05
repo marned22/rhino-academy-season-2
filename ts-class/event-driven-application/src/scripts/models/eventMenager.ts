0e2e25467c1039884fed1d7c03ee8a1df3077139
Martin <martin.elfantastico@gmail.com>
2025-03-05 19:36:40 +0100

    Undone Task2 done Task3

--- ts-class/event-driven-application/src/scripts/models/eventMenager.ts
+++ ts-class/event-driven-application/src/scripts/models/eventMenager.ts
@@ -1,24 +1,40 @@
-type CallBack = () => void
+type Callback = () => void
 
-type Events = {name: string, value: () => []}
+type Events = {[key: string] : [Callback]}
 
 interface IEventMenager{
-    subscribe(event: string, callBack: CallBack): void
+    subscribe(event: string, callBack: Callback): void
     dispatch(event: string, data: string): void
-    unsubscribe(event: string, callBack: CallBack): void
+    unsubscribe(event: string, callBack: Callback): void
     unsubscribeAll(event: string): void
 }
 
 class EventMenager implements IEventMenager{
-    private events: Events[]
-    subscribe(event: string, callBack: CallBack): void {
+    private events: Events = {}
+    private static instance: EventMenager | null
 
+    private constructor() {}
+
+    static getEventMenager(): EventMenager{
+        if(this.instance){
+            const instance = new EventMenager()
+            EventMenager.instance = instance
+        }
+
+        return EventMenager.instance!;
+    }
+
+
+
+    subscribe(event: string, callBack: Callback): void {
+        this.events.
     }
     dispatch(event: string, data: string): void {
-        
+        for 
     }
-    unsubscribe(event: string, callBack: CallBack): void {
-        
+    unsubscribe(event: string, callBack: Callback): void {
+
+        delete this.events.callBack
     }
     unsubscribeAll(event: string): void {
         
