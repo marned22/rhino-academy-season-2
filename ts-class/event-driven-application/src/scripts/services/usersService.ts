dfd2a6302ac9e9240fe0628ed59b1d3af21190c0
Martin <martin.elfantastico@gmail.com>
2025-03-27 16:43:21 +0100

    Task 16

--- ts-class/event-driven-application/src/scripts/services/usersService.ts
+++ ts-class/event-driven-application/src/scripts/services/usersService.ts
@@ -1,17 +1,35 @@
-import { BaseApiService } from "./BaseApiService";
+import { ApiError, BaseApiService } from "./BaseApiService";
 import { v4 as uuidv4 } from "uuid";
 import { UserCredentials } from "../models";
 import { IChatUser } from "../models";
 
 class UserService extends BaseApiService{
     public getAll(): Promise<IChatUser[]> {
-        return this.request<IChatUser[]>('/users');
+        return this.get<IChatUser[]>('/users');
     }
     public getById(id: string){
-        return this.request<IChatUser[]>(`./users/{id}`)
+        return this.get<IChatUser>(`./users/${id}`)
+        .then(user => {
+            if(!user)
+                throw new ApiError(404, 'User not fount')
+            return user
+        })
     }
 
-    public register(credentials: UserCredentials){
-        
+    public register(credentials: UserCredentials): Promise<{ user: IChatUser; token: string}>{
+        const newUser = { id: uuidv4(), ...credentials}
+        return this.post<{user: IChatUser; token: string}>('/auth/register', newUser)
+            .then(response => {
+                if(!response.user) throw new ApiError(400, 'Registration failed')
+                return response
+            })
+    }
+
+    public login(credentials: UserCredentials): Promise<{ user: IChatUser; token: string}>{
+        return this.post<{ user: IChatUser,  token: string}>('/auth/login', credentials)
+            .then(response => {
+                if(!response.user) throw new ApiError(401, 'Invalid username or password')
+                return response
+            })
     }
 }
