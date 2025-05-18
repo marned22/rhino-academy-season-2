d260b987deb606f065a2ae73ce33d26c888e2feb
Dimitar Stefanovski <dimitarstefanovski@Dimitars-MacBook-Air.local>
2025-05-19 00:10:56 +0200

    Clean PR

--- ts-class/creating-an-npm-package/ts-test/dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ts_npm_1 = require("@marned99/ts-npm");
-console.log((0, ts_npm_1.calculator)(5, 5, '+'));
--- ts-class/creating-an-npm-package/ts-test/index.ts
+++
@@ -1,3 +0,0 @@
-const calculator = require('@marned99/ts-npm')
-
-console.log(calculator(5, 5, '+'))
